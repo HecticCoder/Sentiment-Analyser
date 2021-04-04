e8a500b30c4002ec2abab4d0611873501ba058e0
HecticCoder <78785653+HecticCoder@users.noreply.github.com>
2021-04-04 23:06:51 +0530

    Create Sentiment Analysis.py

---
+++ Sentiment Analysis.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+
+st.title('SENTIMENT ANALYSER')
+
+df = pd.read_excel('/content/IMDB-Movie-Reviews-Large-Dataset-50k/train.xlsx')
+
+x = df.iloc[:,1].values
+y = df.iloc[:,0].values
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(max_features=5000)
+x = df['Reviews'].astype('U').values
+y = df['Sentiment']
+x = tfidf.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.3, random_state = 0, stratify = y)
+
+from sklearn.svm import LinearSVC
+clf = LinearSVC()
+clf.fit(x_train, y_train)
+
+select = st.text_input('Enter your review')
+select.replace('\d+', '')
+select.replace('[^\w\s]', '')
+vect = tfidf.transform([select])
+op = clf.predict(vect)
+
+if op == ['pos']:
+  op = 'Positive'
+
+else:
+  op = 'Negative'
+
+if st.button('Predict'):
+  st.title(op)
