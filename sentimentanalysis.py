e2af65bfe5dad1779cad42828b9aace207319862
HecticCoder <78785653+HecticCoder@users.noreply.github.com>
2021-04-05 19:28:26 +0530

    Update sentimentanalysis.py

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title('SENTIMENT ANALYSER')
 
-df = pd.read_excel('https://github.com/HecticCoder/Sentiment-Analyser/blob/main/Sentiment Analysis Data.xlsx')
+df = pd.read_excel('https://github.com/HecticCoder/Sentiment-Analyser/blob/main/data.xlsx')
 
 x = df.iloc[:,1].values
 y = df.iloc[:,0].values
